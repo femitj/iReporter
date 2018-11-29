0b05a4db8295d84ac48f3c9356ccc926345a3a52
Tijani Oluwafemi <olutimedia@gmail.com>
2018-11-29 16:54:56 +0100

    [finishes #162295456] create API endpoint for DELETE a specific red-flag record

--- src/controllers/RedFlagRecord.js
+++ src/controllers/RedFlagRecord.js
@@ -22,6 +22,25 @@ const RedFlagRecord = {
         }
         return res.status(200).send(redFlagRecord);
     },
+
+    update(req, res) {
+        const redFlagRecord = RedFlagRecordModel.findOne(req.params.id);
+        if (!redFlagRecord) {
+          return res.status(404).send({'message': 'red flag record not found'});
+        }
+        const updatedRedFLagRecord = RedFlagRecordModel.update(req.params.id, req.body)
+        return res.status(200).send(updatedRedFlagRecord);
+    },
+
+    delete(req, res) {
+        const redFlagRecord = RedFlagRecordModel.findOne(req.params.id);
+        if (!redFlagRecord) {
+          return res.status(404).send({'message': 'red flag record not found'});
+        }
+        const ref = RedFlagRecordModel.delete(req.params.id);
+        return res.status(204).send(ref);
+      }
+    
 }
 
 export default RedFlagRecord;
--- src/routes/redFlagRecord.js
+++ src/routes/redFlagRecord.js
@@ -12,4 +12,10 @@ router.get('/api/v1/red-flags', RedFlagRecord.getAll);
 // get a specific red-flag record
 router.get('/api/v1/red-flags/:id', RedFlagRecord.getOne);
 
+// edit a specific red-flag record
+router.put('/api/v1/red-flags/:id', RedFlagRecord.update);
+
+// delete a red-flag record
+router.delete('/api/v1/red-flags/:id', RedFlagRecord.delete);
+
 module.exports = router;
