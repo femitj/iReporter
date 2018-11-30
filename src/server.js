7d97a4ad4ba8e01999b29db20d0bda6af6a740c6
Tijani Oluwafemi <olutimedia@gmail.com>
2018-11-30 17:48:46 +0100

    [finishes #162348985] create unit-tests for all API endpoints

--- src/server.js
+++ src/server.js
@@ -2,14 +2,19 @@ import express from 'express';
 import redFlagRoute from './routes/redFlagRecord';
 
 const app = express();
+const PORT = 3000;
+
 
 app.use(express.json());
-app.use('/', express.static(__dirname + '/UI'));
+app.use('/', express.static('UI'));
 
 app.use(redFlagRoute);
 app.get('/api/v1', (req, res) => {
     return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
 })
 
-app.listen(3000)
-console.log('app running on port ', 3000);
+module.exports = app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
+
+//export default server;
