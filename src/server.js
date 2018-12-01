0ab79e2a3013ba02d0892c5aa71be146556b5264
Tijani Oluwafemi <olutimedia@gmail.com>
2018-12-02 00:17:36 +0100

    updated heroku api endpoint

--- src/server.js
+++ src/server.js
@@ -1,18 +1,23 @@
 import express from 'express';
+const bodyParser = require('body-parser');
 import redFlagRoute from './routes/redFlagRecord';
 import dotenv from 'dotenv';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 dotenv.config();
 app.use(express.json());
 app.use('/', express.static('UI'));
 
-app.use(redFlagRoute);
 app.get('/api/v1', (req, res) => {
     return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
 })
+app.use(redFlagRoute);
 
 const server = app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
