7634ba829440662d0a4070596844052ecd590a8f
Tijani Oluwafemi <olutimedia@gmail.com>
2018-12-07 16:48:16 +0100

    [chore #162474470] update validation in patch endpoint

--- src/routes/redFlagRecord.js
+++ src/routes/redFlagRecord.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import RedFlagRecord from '../controllers/RedFlagRecord';
+import checkLocation from '../helpers/checks';
 
 const router = express.Router();
 
@@ -15,7 +16,7 @@ router.get('/api/v1/red-flags/:id', RedFlagRecord.getOne);
 // edit a specific red-flag record.
 router.put('/api/v1/red-flags/:id', RedFlagRecord.update);
 
-router.patch('/api/v1/red-flags/:id/location', RedFlagRecord.updateLocation);
+router.patch('/api/v1/red-flags/:id/location',checkLocation ,RedFlagRecord.updateLocation);
 
 router.patch('/api/v1/red-flags/:id/comment', RedFlagRecord.updateComment);
 
